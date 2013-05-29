93ed80920a7506bd12a93775d4d4049a032b8c8c
Yousuke Utsumi <youtsumi@gmail.com>
2013-05-29 10:23:14 +0900

    Modify error on bit calculation

--- p.pl
+++ p.pl
@@ -7,12 +7,12 @@
 use strict;
 my $verbose=0;
 
-#echosys("gpio load i2c");
+echosys("gpio load i2c");
 echosys("i2cset -y 1 0x60 0x12 0x01");
 my @ret=split(/[\s]+/,`i2cdump -y 1 0x60 i | grep 00: `);
 @ret=@ret[1..16];
-print @ret if $verbose;
-print "\n" if $verbose;
+print STDERR @ret if $verbose;
+print STDERR "\n" if $verbose;
 
 my $padc= getadc(@ret[0,1]);
 my $tadc= getadc(@ret[2,3]);
@@ -21,25 +21,32 @@ my $a0=convcoef(@ret[4,5],1,13,16,0);
 my $b1=convcoef(@ret[6,7],1,3,16,0);
 my $b2=convcoef(@ret[8,9],1,2,16,0);
 my $c12=convcoef(@ret[10,11],1,2,16,9);
-my $c11=convcoef(@ret[12,13],1,1,11,11);
-my $c22=convcoef(@ret[14,15],1,1,11,15);
+my $c11=convcoef(@ret[12,13],1,1,11,12);
+my $c22=convcoef(@ret[14,15],1,1,11,16);
+print STDERR "$a0 $b1 $b2 $c12 $c11 $c22\n" if $verbose;
 
 my $pcomp = $a0+($b1+$c11*$padc+$c12*$tadc)*$padc+($b2+$c22*$tadc)*$tadc;
 printf( "%7.2lf\n",$pcomp);
 
-
 sub convcoef {
 	# convert bits to coefficient of AN3785
+	# padbit must be subtracted -1
 	my ($uw,$lw,$signbit,$digibit,$decimalbit,$padbit) = @_;
 	my $bit = (hex($uw)<<8)|hex($lw);
-	printf( "bit = %s %s %d\n", $uw, $lw, $bit) if $verbose;
-	my $sign=extractbit($bit,0,$signbit)&1?-1:1;
+	printf( STDERR "bit = %s %s %d\n", $uw, $lw, $bit) if $verbose;
+	my $sign=1;
 	my $digit=extractbit($bit,$signbit,$digibit);
 	my $decimal=extractbit($bit,$digibit,$decimalbit);
-	printf( "dec = %d\n", $decimal) if $verbose;
-	my $result = $sign*($digit+$decimal*2.**($digibit-$decimalbit))*10**(-$padbit);
+	printf( STDERR "dec = %d\n", $decimal) if $verbose;
+	my $tmpbit = $digit<<($decimalbit-$digibit)|$decimal;
+	if(extractbit($bit,0,$signbit)&1) {
+		$sign=-1;
+		my $mask = ~(0xFFFF<<($decimalbit-$signbit))&0xFFFF;
+		$tmpbit = (~$tmpbit | 1) & $mask;
+	}
+	my $result = $sign*($tmpbit*2.**(-($decimalbit-$digibit+$padbit)));
 	
-	print "$result\n" if $verbose;
+	print STDERR "$result\n" if $verbose;
 
 	return $result;
 }
@@ -48,13 +55,13 @@ sub extractbit {
 	# internal subroutine for convcoef
 	my ($bit,$stx,$etx) = @_;
 	my $result=0;
-	printf( "%d, %d\n", $stx, $etx) if $verbose;
+	printf( STDERR "%d, %d\n2nd: ", $stx, $etx) if $verbose;
 	for(my $i=$stx;$i<$etx;$i++) {
 		my $j=$i-$stx;
 		$result|=(($bit&(1<<(15-$i)))?1:0)<<($etx-($i+1));
-		printf("%d", (($bit&(1<<(15-$i)))?1:0)) if $verbose;
+		printf( STDERR "%d", (($bit&(1<<(15-$i)))?1:0)) if $verbose;
 	}
-	printf("\n%d\n", $result) if $verbose;
+	printf( STDERR "\n%d\n", $result) if $verbose;
 	return $result;
 }
 
@@ -63,6 +70,6 @@ sub getadc {
 }
 
 sub echosys {
-#	print "@_\n";
+	print STDERR "@_\n" if $verbose;
 	system(@_);
 }
